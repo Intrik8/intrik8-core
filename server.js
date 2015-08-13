7895eb4108ba06d7102157d639272bc7a19a710b
Jason Scherer <jason.scherer.1984@gmail.com>
2015-08-12 22:51:07 -0500

    Worked on server

--- server.js
+++ server.js
@@ -4,23 +4,31 @@
 */
 'use strict';
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
 var app = express();
 var port = process.env.PORT || 8080;
 var router = express.Router();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+  extended: true
+  }));
+app.use(bodyParser.json());
 
 router.use(function(req, res, next) {
-
-    // log each request to the console
     console.log(req.method, req.url);
-
-    // continue doing what we were doing and go to the route
     next();
 });
 
 
 router.get('/', function(req, res){
-  res.send('this is a test');
+  res.render('main/index', {title: 'hey', message: 'Hello'});
 });
+app.use('/', router);
 
 app.listen(port);
 console.log('Server started at port ' + port)
